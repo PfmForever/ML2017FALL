abf41265f426de882ba53c6072fc9d16487c5894
Leo <r06944034@g.ntu.edu.tw>
2017-12-22 20:44:44 +0800

    update test.py upload model

--- hw5/test.py
+++ hw5/test.py
@@ -5,14 +5,15 @@ from keras.models import Model, load_model
 import numpy as np
 import csv
 import pickle
+import pandas as pd
 def rmse(y_true, y_pred):
     return BK.sqrt(BK.mean((y_pred-y_true) ** 2))
 
-reader = csv.reader(open(sys.argv[1]))
-test_data = list(reader)
-test_data = np.array(test_data[1:], dtype = np.dtype('float64'))
+reader = pd.read_csv(open(sys.argv[1]))
+test_data = (reader.values)[:, 1:3]
+test_data = test_data.astype('float64')
 model = load_model('./no_normalize_model.15-0.8473.h5', custom_objects = {'rmse':rmse})
-ans = model.predict(np.hsplit(test_data[:,1:], 2))
+ans = model.predict(np.hsplit(test_data, 2))
 
 
 count = 0
